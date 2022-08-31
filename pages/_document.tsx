1d7af0b4b4c6367356ba8c6b5dbfbba68f43b11a
Varun Jindal <vjindal0112@gmail.com>
2022-08-31 18:50:32 -0400

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,11 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic"
                         rel="stylesheet"
                     />
                 </Head>
-                <body style={{ "fontFamily": "Roboto-light" }}>
+                <body style={{ "fontFamily": "Roboto", "fontWeight": "300" }}>
                     <Main />
                     <NextScript />
                 </body>
