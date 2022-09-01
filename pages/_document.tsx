4288af347d322de3c42c991b7d97de5120b350b3
Varun Jindal <vjindal0112@gmail.com>
2022-08-31 20:16:37 -0400

    custom variable font and seo fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,37 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic"
-                        rel="stylesheet"
-                    />
-                </Head>
-                <body style={{ "fontFamily": "Roboto", "fontWeight": "300" }}>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Product builder, engineer, dancer, foodie — Varun Jindal" />
+          <link rel="canonical" href="https://www.varunjindal.com" />
+          <meta name="robots" content="index, follow" />
+          <link rel="icon" href="/logo.ico" />
+
+          {/* Open Graph tags */}
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Product builder, engineer, dancer, foodie — Varun Jindal" />
+          <meta property="og:image" content="/profile_pic.jpg" />
+          <meta property="og:url" content="https://www.varunjindal.com" />
+          <meta property="og:site_name" content="Varun Jindal" />
+
+          {/* Twitter Tags */}
+          <meta name="twitter:description" content="Product builder, engineer, dancer, foodie — Varun Jindal" />
+          <meta name="twitter:image" content="/profile_pic.jpg" />
+          <meta name="twitter:site" content="@varun_jindal8" />
+          <meta name="twitter:creator" content="@varun_jindal8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
