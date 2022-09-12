04ae98277dd63d6baa1b77343e744caf397896af
Varun Jindal <vjindal0112@gmail.com>
2022-09-12 15:04:40 -0400

    prettier + vipassana video link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link'
-import Layout from '../components/ui/Layout'
-import ProfilePicture from '../components/ui/ProfilePicture'
+import Link from "next/link";
+import Layout from "../components/ui/Layout";
+import ProfilePicture from "../components/ui/ProfilePicture";
 
 const IndexPage = () => (
   <Layout title="Varun Jindal">
@@ -11,39 +11,74 @@ const IndexPage = () => (
     <h2>About Me</h2>
     <p>What I'm up to</p>
     <ul>
-      <li>Product and Eng @ <Link href="https://warp.dev">Warp</Link></li>
+      <li>
+        Product and Eng @ <Link href="https://warp.dev">Warp</Link>
+      </li>
       <li>Built products used by many:</li>
       <ul>
-        <li><Link href="https://try.studybuddies.ai">StudyBuddies</Link></li>
-        <li><Link href="https://enlight.nyc">Enlight</Link></li>
+        <li>
+          <Link href="https://try.studybuddies.ai">StudyBuddies</Link>
+        </li>
+        <li>
+          <Link href="https://enlight.nyc">Enlight</Link>
+        </li>
       </ul>
-      <li>Currently upskilling in code scalability and in... crypto??? <Link href="https://forms.gle/gXT5gMWN4nQmVigHA">Help me out</Link></li>
+      <li>
+        Currently upskilling in code scalability and in... crypto???{" "}
+        <Link href="https://forms.gle/gXT5gMWN4nQmVigHA">Help me out</Link>
+      </li>
       <li>Adventure and adrenaline excite me: roller coasters, skydiving, cliff jumping, etc.</li>
       <li>I like being active: running, soccer, etc.</li>
     </ul>
     <h2>Lessons</h2>
     <p>Some thing I've learned</p>
     <ul>
-      <li>You will never always feel incredible. Don’t over-optimize your life around feeling incredible, over-optimize your life to perform at your best no matter how you are feeling.</li>
+      <li>
+        You will never always feel incredible. Don’t over-optimize your life around feeling incredible, over-optimize
+        your life to perform at your best no matter how you are feeling.
+      </li>
       <li>There’s no limit to willpower; there’s no quota for the day.</li>
       <li>Show up for your friends, always.</li>
       <li>Everyone is still figuring stuff out. No one has their life together, no matter how successful they are. </li>
       <li>Single minded focus is the most powerful force in the universe.</li>
       <li>Inspiration is fleeting, act on it immediately. </li>
-      <li>The mismatch of how difficult you expect something to be and how hard it is in actuality is the source of most of your anger and frustration.</li>
+      <li>
+        The mismatch of how difficult you expect something to be and how hard it is in actuality is the source of most
+        of your anger and frustration.
+      </li>
       <li>Friendship, at its core, is mutual mentorship.</li>
-      <li>Always assume people are acting in good faith. Even if they aren’t, your perception and reaction will be more true to yourself.</li>
+      <li>
+        Always assume people are acting in good faith. Even if they aren’t, your perception and reaction will be more
+        true to yourself.
+      </li>
     </ul>
     <h2>Links</h2>
     <p>Links to stuff</p>
     <ul>
-      <li><Link href="https://projects.varunjindal.com">Wall of projects</Link></li>
-      <li><Link href="https://lessons.varunjindal.com">Full list of lessons</Link></li>
-      <li><Link href="https://www.notion.so/varunjindal/a57d3289125c4ded9ab9fe7b0452d35c?v=b5c1b23a568c4cbb8f55451cc4398b5a">Lessons from working at Collab AI</Link></li>
-      <li><Link href="https://www.michigandaily.com/campus-life/student-run-studybuddies-site-helps-students-connect-virtual-classes/">StudyBuddies in the Michigan Daily</Link></li>
-      <li><Link href="https://youtu.be/Cd_nKLzmCuA">Kitty Cats in Greece</Link></li>
+      <li>
+        <Link href="https://projects.varunjindal.com">Wall of projects</Link>
+      </li>
+      <li>
+        <Link href="https://lessons.varunjindal.com">Full list of lessons</Link>
+      </li>
+      <li>
+        <Link href="https://www.notion.so/varunjindal/a57d3289125c4ded9ab9fe7b0452d35c?v=b5c1b23a568c4cbb8f55451cc4398b5a">
+          Lessons from working at Collab AI
+        </Link>
+      </li>
+      <li>
+        <Link href="https://www.michigandaily.com/campus-life/student-run-studybuddies-site-helps-students-connect-virtual-classes/">
+          StudyBuddies in the Michigan Daily
+        </Link>
+      </li>
+      <li>
+        <Link href="https://youtu.be/Cd_nKLzmCuA">Kitty Cats in Greece</Link>
+      </li>
+      <li>
+        <Link href="https://youtu.be/jIC0aW7ZR9o">Living like a monk for 10 days</Link>
+      </li>
     </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
